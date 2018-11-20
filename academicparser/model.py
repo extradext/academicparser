211db606149b54761188e67e1a9a86bd537339cd
am2rican5 <am2rican5@gmail.com>
2018-11-20 17:50:46 +0900

    added citation string parser

--- academicparser/model.py
+++ academicparser/model.py
@@ -1,18 +1,51 @@
+import urllib.request
+import urllib.parse
 from dataclasses import dataclass, field
 from enum import Enum
 from typing import List, Optional
+
+from bs4 import BeautifulSoup
+
+
+REQUEST_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
 
 class PaperType(Enum):
     PAPER_TYPE_PMC = 'PMC'
     PAPER_TYPE_ETC = 'ETC'
 
+
 @dataclass
-class ReferenceString:
-    ref_string: str = ''
+class CitationString:
+    raw_string: str
+    authors: Optional[List[str]] = None
+    title: Optional[str] = None
+    year: Optional[str] = None
     journal: Optional[str] = None
+    publisher: Optional[str] = None
+    book_title: Optional[str] = None
     volume: Optional[str] = None
+    note: Optional[str] = None
     pubmed_path: Optional[str] = None
+    pmc_free_article: Optional[str] = None
+
+    def parse(self):
+        req_data = 'citation={}'.format(self.raw_string).encode('utf-8')
+        req = urllib.request.Request('http://freecite.library.brown.edu/citations/create',
+                                     data=req_data, headers={'Accept': 'text/xml'}, method='POST')
+        with urllib.request.urlopen(req) as f:
+            body = f.read().decode('utf-8')
+            soup = BeautifulSoup(body, 'xml')
+            self.authors = [e.string.strip() for e in soup.find_all('author')]
+
+            get_string = lambda x: x.string.strip() if x is not None else None
+            self.title = get_string(soup.find('title'))
+            self.year = get_string(soup.find('year'))
+            self.journal = get_string(soup.find('journal'))
+            self.publisher = get_string(soup.find('publisher'))
+            self.book_title = get_string(soup.find('booktitle'))
+            self.volume = get_string(soup.find('volume'))
+            self.note = get_string(soup.find('note'))
 
 @dataclass
 class Paper:
@@ -20,7 +53,6 @@ class Paper:
     title: str = ''
     authors: List[str] = field(default_factory=list)
     abstract: str = ''
-    references: List[ReferenceString] = field(default_factory=list)
+    references: List[CitationString] = field(default_factory=list)
     pmid: Optional[int] = None
     pmcid: Optional[str] = None
-
--- academicparser/pmc.py
+++ academicparser/pmc.py
@@ -1,46 +1,62 @@
 import urllib.request
-from html.parser import HTMLParser
+import urllib.parse
 from typing import Optional
 
 from bs4 import BeautifulSoup
 
-from academicparser.model import Paper, PaperType, ReferenceString
+from academicparser.model import Paper, PaperType, CitationString
 
 REQUEST_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 PMC_PAPER_URL_FORMAT = 'https://www.ncbi.nlm.nih.gov/pmc/articles/{}/'
 
-def parse(pmcid: str) -> Optional[Paper]:
+def parse(pmcid: str, parse_cite_string: bool = False) -> Optional[Paper]:
     paper = Paper(PaperType.PAPER_TYPE_PMC)
     paper.pmcid = pmcid
 
     paper_url: str = PMC_PAPER_URL_FORMAT.format(pmcid)
 
-    req = urllib.request.Request(paper_url)
-    req.add_header('User-Agent', REQUEST_USER_AGENT)
+    req = urllib.request.Request(paper_url, headers={'User-Agent': REQUEST_USER_AGENT})
     with urllib.request.urlopen(req) as f:
-        if f.status == 200:
-            content_type = f.info()['Content-Type']
-            charset = content_type.split(';')[-1].split('=')[-1]
+        content_type = f.info()['Content-Type']
+        charset = content_type.split(';')[-1].split('=')[-1]
 
-            res_body = f.read().decode(charset)
+        res_body = f.read().decode(charset)
 
-            soup = BeautifulSoup(res_body, 'html.parser')
-            paper.title = soup.find('h1', class_='content-title').string
+        soup = BeautifulSoup(res_body, 'html.parser')
+        paper.title = soup.find('h1', class_='content-title').string
 
-            authors_container = soup.find('div', class_='contrib-group fm-author')
-            paper.authors += [e.string for e in authors_container.find_all('a')]
+        authors_container = soup.find('div', class_='contrib-group fm-author')
+        paper.authors += [e.string for e in authors_container.find_all('a')]
 
-            paper.abstract = soup.find('h2', text='Abstract').next_sibling.string
+        abstract_header = soup.find('h2', text='Abstract')
+        if abstract_header is not None:
+            abstract_container = abstract_header.parent.find('p')
+            if abstract_container is not None:
+                paper.abstract = abstract_container.string
 
-            ref_container = soup.find('ul', class_='back-ref-list')
-            for e in ref_container.select('li > span'):
-                ref = e.get_text().replace(' [PubMed]', '')
-                ref_string = ReferenceString(ref)
+        ref_container = soup.find('div', class_='ref-list-sec sec')
+        sel_ref_container = ref_container.find('ul', class_='back-ref-list')
 
-                ref_string.journal = e.find('span', class_='ref-journal').string
-                ref_string.volume = e.find('span', class_='ref-vol').string
-                ref_string.pubmed_path = e.find('a').get('href')
+        if sel_ref_container is not None:
+            ref_spans = sel_ref_container.select('li > span')
+        else:
+            ref_spans = ref_container.select('div > span')
+        for e in ref_spans:
+            citation_string_raw = e.get_text()
+            citation_string_raw = citation_string_raw.replace(' [PubMed]', '').replace(' [PMC free article]', '')
 
-                paper.references.append(ref_string)
+            citation_string = CitationString(citation_string_raw)
+
+            for a in e.find_all('a'):
+                if a.string == 'PMC free article':
+                    citation_string.pmc_free_article = a.get('href')
+                elif a.string == 'PubMed':
+                    citation_string.pubmed_path = a.get('href')
+
+            paper.references.append(citation_string)
+
+    if parse_cite_string:
+        for r in paper.references:
+            r.parse()
 
     return paper
